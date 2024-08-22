8d5a3a8ff5ff8c43488f6944381339aac2d8e978
sunshine-web-coder <webcoderdev02@gmail.com>
2024-08-22 16:02:42 +0100

    image and alicemulticolumn page translate to german

--- app/components/AliceMultiColumn/AliceMultiColumn.js
+++ app/components/AliceMultiColumn/AliceMultiColumn.js
@@ -39,63 +39,64 @@ export default function AliceMultiColumn() {
     <>
       <section ref={ref} className="alice-multi-column relative z-10 md:pt-[70px] pt-0">
         <Image className="absolute left-0 top-0 w-full -z-10 md:block hidden" src={backgroundShape} alt="Hintergrundform" />
-        <Image className="absolute right-0 top-[50px] w-1/2 -z-10 md:block hidden" src={MachineThumb} alt="Maschinendaumen" />
         <div className="container">
           {/* erste Spalte */}
           <motion.div className="grid md:grid-cols-2" variants={stagger} initial="hidden" animate={controls}>
             <motion.div variants={fadeInUp}>
               <h2 className="2xl:text-[48px] lg:text-[36px] text-[26px] font-normal md:mb-[15px] mb-[8px]">Bequeme Zahlungen mit Alice 1</h2>
-              <p className="lg:text-[24px] md:text-[18px] text-[14px] md:mb-[35px] mb-[30px]">Wir haben Alice 1 entwickelt, um Ihre POS- und Einzelhandelstransaktionen nahtlos und schnell zu gestalten, egal wo Sie weltweit unterwegs sind.</p>
+              <p className="lg:text-[24px] md:text-[18px] text-[14px] md:mb-[35px] mb-[30px]">Wir haben Alice 1 entwickelt, um Ihre POS- und Einzelhandelszahlungen weltweit nahtlos und schnell zu gestalten.</p>
               <ul className="flex flex-col md:gap-[30px] gap-[15px] lg:w-[85%] w-full">
                 <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-white border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] mr-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
-                  Einmal registrieren und für Ihre Lieblingsartikel für immer, überall und jederzeit bezahlen
+                  Einmal registrieren und für Ihre Lieblingsartikel überall, jederzeit bezahlen
                 </motion.li>
                 <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-white border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] ml-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
-                  Es ist kontaktlos und sicher: Sie müssen nur Ihre Handfläche über das POS-Gerät halten
+                  Kontaktlos und sicher: Sie müssen nur Ihre Handfläche auf das POS-Gerät halten
                 </motion.li>
                 <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-white border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] mr-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
-                  Sie haben die volle Kontrolle: Sie entscheiden, wo und wann Sie Ihre Handflächen-ID verwenden oder genehmigen möchten
+                  Sie haben die volle Kontrolle: Sie entscheiden, wo und wann Sie Ihre Handflächen-ID verwenden oder genehmigen
                 </motion.li>
               </ul>
-              <Image className="sm:hidden block ml-auto -mr-[15px] mt-[30px]" src={MachineThumbMobile} alt="Maschinendaumen Mobil" />
             </motion.div>
+            <div className="md:ml-auto mt-[30px] md:mt-0">
+              <Image className="" src={MachineThumb} alt="Zahlungsmaschine" />
+            </div>
           </motion.div>
           {/* zweite Spalte */}
           <motion.div className="grid md:grid-cols-2 gap-[40px] md:mt-[-100px] mt-[60px]" variants={stagger} initial="hidden" animate={controls}>
             <motion.div className="mt-[150px] md:block hidden" variants={fadeInUp}>
-              <Image className="" src={SecurityThumb} alt="Sicherheitsdaumen" />
+              <Image className="" src={SecurityThumb} alt="Sicherheitsbild" />
             </motion.div>
             <motion.div variants={fadeInUp}>
               <h2 className="2xl:text-[48px] lg:text-[36px] text-[26px] font-normal mb-[30px]">Nahtlose Zugangskontrolle mit Alice 2</h2>
               <ul className="flex flex-col md:gap-[30px] gap-[15px] lg:w-[90%] w-full">
-                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] mr-[50px]" variants={fadeInUp}>
+                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A0EFA6] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] mr-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
-                  Mit Alice 2 können Sie problemlos durch jeden eingeschränkten Bereich, Eingang, Ressource oder Informationen gelangen.
+                  Durchqueren Sie mühelos jeden eingeschränkten Ort, Eingang, Ressource oder Informationen mit Alice 2.
                 </motion.li>
-                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] ml-[50px]" variants={fadeInUp}>
+                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A0EFA6] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] ml-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
                   Es ist schnell, flexibel und sicher: Sie autorisieren den Zugang nur mit Ihrer Handfläche.
                 </motion.li>
-                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] mr-[50px]" variants={fadeInUp}>
+                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A0EFA6] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] mr-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
-                  Jeder hat eine einzigartige Identität, was zusätzliche Zuverlässigkeit und Sicherheit gewährleistet.
+                  Jeder hat eine einzigartige Identität, was zusätzliche Zuverlässigkeit und Sicherheit bietet.
                 </motion.li>
-                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A9ED12] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] ml-[50px]" variants={fadeInUp}>
+                <motion.li className="flex lg:gap-[15px] gap-[10px] md:text-[20px] text-[14px] bg-[#CAF7CE] border border-[#A0EFA6] md:rounded-[20px] rounded-[15px] py-[20px] px-[15px] ml-[50px]" variants={fadeInUp}>
                   <span className="text-[#424242] text-[16px] mt-[5px]">
                     <IoMdCheckmarkCircle />
                   </span>
