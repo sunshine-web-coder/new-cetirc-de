3db6e3eb00a8d1ee4df3ffa3af66d255511798d9
sunshine-web-coder <webcoderdev02@gmail.com>
2024-09-19 14:17:32 +0100

    ss

--- app/components/AliceWork1/AliceWork1.js
+++ app/components/AliceWork1/AliceWork1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import AliceWorkThumb1 from '@/app/assets/images/alice1work-thumb-n.png';
+import AliceWorkThumb1 from '@/app/assets/images/alice1work-thumb-n.jpg';
 import AliceWorkThumbMobile from '@/app/assets/images/alice1work-thumb-mobile.png';
 
 export default function AliceWork1() {
@@ -17,8 +17,8 @@ export default function AliceWork1() {
             </p>
           </div>
           <div className="grid sm:grid-cols-[4fr_8fr] lg:gap-x-[60px] gap-x-[30px]">
-            <div className="mb-[30px] flex items-center justify-center bg-[#333333] rounded-[15px] overflow-hidden">
-              <Image className="w-[350px]" src={AliceWorkThumb1} alt="AliceWorkThumb1" />
+            <div className="w-full sm:w-[450px] mb-[30px] flex items-center justify-center bg-[#ffffff] rounded-[15px] overflow-hidden">
+              <Image className="w-full h-full" src={AliceWorkThumb1} alt="AliceWorkThumb1" />
             </div>
             <div className="relative">
               {/* single work item  */}
